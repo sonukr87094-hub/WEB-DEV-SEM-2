131b62f25d8076f1e6f2642eff5da8eba6544fb6
Sonu Kumar <sonukr87094@gmail.com>
2026-05-20 19:11:48 +0530

    ASSESSMENT

---
+++ CLASS ASSESSMENT/ASSESSMENT 8/counter-app/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // State creation
+  const [count, setCount] = useState(0);
+
+  // Increment function
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Decrement function
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  // Reset function
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <h1>Counter App</h1>
+
+      {/* Display Count */}
+      <h2>{count}</h2>
+
+      {/* Buttons */}
+      <div className="buttons">
+        <button onClick={increment}>+</button>
+
+        <button onClick={decrement}>-</button>
+
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
